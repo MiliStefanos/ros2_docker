137215b764e328625ce00e2f942598843af62384
Stefanos Miliotis <milistefanos@hotmail.com>
2025-10-25 23:59:00 +0300

    Yaml file, client, server

--- src/linear_algebra_service/src/least_squares_client.cpp
+++ src/linear_algebra_service/src/least_squares_client.cpp
@@ -1,28 +1,64 @@
 #include "rclcpp/rclcpp.hpp"
 #include "linear_algebra_service/srv/least_squares.hpp"
 
+#include <yaml-cpp/yaml.h>
+#include <geometry_msgs/msg/vector3.hpp>
 #include <chrono>
 #include <cstdlib>
 #include <memory>
 
 using namespace std::chrono_literals;
 
+struct InputData
+{
+  std::vector<geometry_msgs::msg::Vector3> m_matrix{};
+  geometry_msgs::msg::Vector3 vector;
+};
+
+InputData ParseYamlFile()
+{
+  YAML::Node config = YAML::LoadFile("/ros2_ws/src/linear_algebra_service/config/input.yaml");
+  if (!config["matrix"] || !config["vector"]) {
+    throw std::runtime_error("Missing keys in input.yaml");
+  }
+
+  std::vector<geometry_msgs::msg::Vector3> matrix;
+  RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Mx3 matrix: ");
+  for (const auto& row : config["matrix"]) {
+    geometry_msgs::msg::Vector3 v;
+    v.x = row[0].as<double>();
+    v.y = row[1].as<double>();
+    v.z = row[2].as<double>();
+    matrix.push_back(v);
+    RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "[%.1f, %.1f, %.1f]", v.x, v.y, v.z);
+  }
+
+  auto vec = config["vector"];
+  geometry_msgs::msg::Vector3 v;
+  v.x = vec[0].as<double>();
+  v.y = vec[1].as<double>();
+  v.z = vec[2].as<double>();
+  RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "vector: [%.1f, %.1f, %.1f]", v.x, v.y, v.z);
+  return InputData{matrix, v};
+}
+
 int main(int argc, char **argv)
 {
   rclcpp::init(argc, argv);
 
-  if (argc != 3) {
-      RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "usage: least_squares_client X Y");
-      return 1;
-  }
+  RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "stleast_squares_client started");
 
   std::shared_ptr<rclcpp::Node> node = rclcpp::Node::make_shared("least_squares_client");
   rclcpp::Client<linear_algebra_service::srv::LeastSquares>::SharedPtr client =
     node->create_client<linear_algebra_service::srv::LeastSquares>("least_squares");
 
+  // Load params from yaml file
+  auto inputData = ParseYamlFile();
+
+  // Call server with input Data
   auto request = std::make_shared<linear_algebra_service::srv::LeastSquares::Request>();
-  request->a = atoll(argv[1]);
-  request->b = atoll(argv[2]);
+  request->matrix = inputData.m_matrix;
+  request->vector = inputData.vector;
 
   while (!client->wait_for_service(1s)) {
     if (!rclcpp::ok()) {
@@ -33,14 +69,24 @@ int main(int argc, char **argv)
   }
 
   auto result = client->async_send_request(request);
-  // Wait for the result.
+  //Wait for the result.
   if (rclcpp::spin_until_future_complete(node, result) ==
     rclcpp::FutureReturnCode::SUCCESS)
   {
-    RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Sum: %ld", result.get()->sum);
+    RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Client received response from server");
   } else {
-    RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "Failed to call service least_squares");
+    RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "Failed to call service");
   }
+
+  //auto result = client->async_send_request(request);
+  // Wait for the result.
+  // if (rclcpp::spin_until_future_complete(node, result) ==
+  //   rclcpp::FutureReturnCode::SUCCESS)
+  // {
+  //   RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Sum: %ld", result.get()->sum);
+  // } else {
+  //   RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "Failed to call service least_squares");
+  // }
 
   rclcpp::shutdown();
   return 0;
--- src/linear_algebra_service/src/least_squares_server.cpp
+++ src/linear_algebra_service/src/least_squares_server.cpp
@@ -1,15 +1,31 @@
 #include "rclcpp/rclcpp.hpp"
 #include "linear_algebra_service/srv/least_squares.hpp"
+#include <Eigen/Dense>
 
 #include <memory>
 
-void add(const std::shared_ptr<linear_algebra_service::srv::LeastSquares::Request> request,
+void calculateLeastSquares(const std::shared_ptr<linear_algebra_service::srv::LeastSquares::Request> request,
           std::shared_ptr<linear_algebra_service::srv::LeastSquares::Response> response)
 {
-  response->sum = request->a + request->b;
-  RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Incoming request\na: %ld" " b: %ld",
-                request->a, request->b);
-  RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "sending back response: [%ld]", (long int)response->sum);
+  RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Service received from client");
+  RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "matrix:");
+  for (const auto& row : request->matrix) {
+    RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "[%.1f, %.1f, %.1f]", row.x, row.y, row.z);
+  }
+  RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "3d_vector: [%.1f, %.1f, %.1f]", request->vector.x, request->vector.y, request->vector.z);
+
+
+  Eigen::MatrixXf A = Eigen::MatrixXf::Random(3, 2);
+  std::cout << "Here is the matrix A:\n" << A << std::endl;
+  Eigen::VectorXf b = Eigen::VectorXf::Random(3);
+  std::cout << "Here is the right hand side b:\n" << b << std::endl;
+  std::cout << "The least-squares solution is:\n"
+            << A.bdcSvd(Eigen::ComputeThinU | Eigen::ComputeThinV).solve(b) << std::endl;
+  // Calculate 
+  // response->sum = request->a + request->b;
+  // RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Incoming request\na: %ld" " b: %ld",
+  //               request->a, request->b);
+  // RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "sending back response: [%ld]", (long int)response->sum);
 }
 
 int main(int argc, char **argv)
@@ -20,9 +36,9 @@ int main(int argc, char **argv)
   // Create a node
   std::shared_ptr<rclcpp::Node> node = rclcpp::Node::make_shared("least_squares_server");
 
-  // Creates a service named least_squares for that node and automatically advertises it over the networks with the &add method:
+  // Creates a service named least_squares for that node and automatically advertises it over the networks with the &calculateLeastSquares method:
   rclcpp::Service<linear_algebra_service::srv::LeastSquares>::SharedPtr service =
-    node->create_service<linear_algebra_service::srv::LeastSquares>("least_squares", &add);
+    node->create_service<linear_algebra_service::srv::LeastSquares>("least_squares", &calculateLeastSquares);
 
   RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Ready to calculate least squares.");
 
